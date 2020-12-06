e448ecec9829f80de5e6510d0aa7344079047661
edigonzales <edi.gonzales@gmail.com>
2020-12-06 17:10:28 +0100

    [lambda] initial commit

---
+++ lambda/src/main/java/ch/so/agi/ilivalidator/lambda/IlivalidatorLambdaApplication.java
@@ -0,0 +1,13 @@
+package ch.so.agi.ilivalidator.lambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlivalidatorLambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlivalidatorLambdaApplication.class, args);
+	}
+
+}
---
+++ lambda/src/test/java/ch/so/agi/ilivalidator/lambda/IlivalidatorLambdaApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.so.agi.ilivalidator.lambda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlivalidatorLambdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
